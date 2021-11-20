8398c3509a1c92657536f00717d5778675205197
Willow Gavette <zoey.m.gavette@gmail.com>
2021-11-19 16:11:18 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+const App = () => {
+	return (
+		<header>
+			<h1>WILLOW GAVETTE</h1>
+			<h2>Full-Stack Software Engineer</h2>
+		</header>
+	);
+}
+
+export default function App();
---
+++ my-website-react-redesign/src/App.js
@@ -0,0 +1,15 @@
+import './index.css';
+import AboutMe from './AboutMe.js';
+
+function App() {
+  return (
+    <div className="header">
+      <header>
+        <h1>WILLOW GAVETTE</h1>
+        <h2>Full-Stack Software Engineer</h2>
+      </header>
+    </div>
+  );
+};
+
+export default App;
