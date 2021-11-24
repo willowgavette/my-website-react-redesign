83e81e81bf13fcea6f248714d8e7bc738c7e6539
Willow Gavette <zoey.m.gavette@gmail.com>
2021-11-23 18:25:53 -0800

    selfie works now

--- my-website-react-redesign/src/App.js
+++ my-website-react-redesign/src/App.js
@@ -57,7 +57,7 @@ function Contact () {
   let {contact: { tel, email }} = data;
   return (
     <div className="contact">
-        <img src={require('./self.jpg')}></img>
+        <img src={self}></img>
         <h4>Contact Information</h4>
         <a href={email}><p align="center">&#128231; Email: zoey.m.gavette@gmail.com</p></a>
         <a href={tel}><p align="center">&#9742;&#65039; Phone: (503) 820-9509</p></a>
