fd4e0a40298f34732246ac640f9270de4c14155b
Willow Gavette <zoey.m.gavette@gmail.com>
2021-11-23 18:21:00 -0800

    self.jpg not loading

--- my-website-react-redesign/src/App.js
+++ my-website-react-redesign/src/App.js
@@ -1,5 +1,6 @@
 import './index.css';
 import data from './info.json';
+import self from './self.jpg';
 
 function App () {
   let { name, title } = data;
@@ -12,7 +13,9 @@ function App () {
     </header>
     </div>
 
-    <AboutMe />
+    <SideBySide />
+
+    <Links />
     </>
     );
 };
@@ -20,12 +23,19 @@ function App () {
 function AboutMe () {
   let {about: {firstDesc, secondDesc}} = data;
   return (
-    <div>
-      <h2>ABOUT ME</h2>
-      <p>{firstDesc}</p>
-      <p>{secondDesc}</p>
+    <div className="bio">
+    <h2>ABOUT ME</h2>
+    <p>{firstDesc}</p>
+    <p>{secondDesc}</p>
+    </div>
+    );
+};
 
-      <Links />
+function SideBySide () {
+  return (
+    <div className="sides">
+    <AboutMe />
+    <Contact />
     </div>
     );
 };
@@ -33,8 +43,26 @@ function AboutMe () {
 function Links () {
   let {links: {gitHub, djangoApp, linkedIn}} = data;
   return (
-    {links.map(link, i) => (
-      <h2><a key={i} href={link}>My {i}</a></h2>))
-}
+//     {links.map(link, i) => (
+//       <h2><a key={i} href={link}>blah</a></h2>))
+// }
+<><h2><a href={gitHub}>My GitHub Portfolio</a></h2>
+<h2><a href={djangoApp}>My Django Reading List app</a></h2>
+<h2><a href={linkedIn}>My LinkedIn</a></h2>
+</>
+);
+};
+
+function Contact () {
+  let {contact: { tel, email }} = data;
+  return (
+    <div className="contact">
+        <img src={require('./self.jpg')}></img>
+        <h4>Contact Information</h4>
+        <a href={email}><p align="center">&#128231; Email: zoey.m.gavette@gmail.com</p></a>
+        <a href={tel}><p align="center">&#9742;&#65039; Phone: (503) 820-9509</p></a>
+      </div>
+      );
+};
 
 export default App;
