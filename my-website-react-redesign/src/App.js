894edbd7261b07bf7c445aeb1c9eb1ba6202a648
Willow Gavette <zoey.m.gavette@gmail.com>
2021-11-29 15:26:02 -0800

    all content present on page

--- my-website-react-redesign/src/App.js
+++ my-website-react-redesign/src/App.js
@@ -16,6 +16,10 @@ function App () {
     <SideBySide />
 
     <Links />
+
+    <Quals />
+
+    <Footer />
     </>
     );
 };
@@ -63,6 +67,39 @@ function Contact () {
         <a href={tel}><p align="center">&#9742;&#65039; Phone: (503) 820-9509</p></a>
       </div>
       );
+};
+
+function Quals () {
+  return (
+    <div class="quals">
+        <h2>QUALIFICATIONS</h2>
+        <h3>Programming Languages</h3>
+        <p><b>- Python</b></p>
+        <p><b>- JavaScript</b></p>
+        <p><b>- C++</b></p>
+        <h3>Web Frameworks</h3>
+        <p><b>- Django</b></p>
+        <h3>Markup Languages</h3>
+        <p><b>- HTML5</b></p>
+        <p><b>- CSS3</b></p>
+        <h3>Operating System Environments</h3>
+        <p><b>- Linux OSes with PyCharm and Sublime Text</b></p>
+        <p><b>- Windows OSes with Visual Studio and Spyder</b></p>
+        <h2>EDUCATION</h2>
+        <p><b>- Graduated from Portland Community College in 2014 with an Associate's Degree in Computer Science</b></p>
+        <p><b>- Graduated University of Oregon in 2020 with a Bachelor's of Science in Psychology</b></p>
+      </div>
+      );
+};
+
+function Footer () {
+  let { thanks, cred } = data;
+  return (
+      <footer>
+      <h2>{thanks}</h2>
+      <small><center>{cred}</center></small>
+      </footer>
+    );
 };
 
 export default App;
